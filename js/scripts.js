c2f0bc9b56e61c4009e0859b74aad734af923960
Sam Cook <WhenPotatoesFry@gmail.com>
2019-06-21 10:28:16 -0700

    create 2 forms for the user to fill out to specify what the user wants on his pizza and add the javascript to make the buttons associated with the forms work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,38 @@
+// order object contains everything in the user's order
+function Order() {
+  this.pizzas = [],
+  this.drinks = [],
+  this.total = 0,
+  this.currentPizza = 0;
+}
+
 // pizza object. pass it the user's name so we know who's order it is
 function Pizza() {
   // array of meat toppings
-  this.meatToppings = ['anchovies', 'bacon', 'chicken'];
+  this.meatToppings = [],
   // array of other toppings
-  this.otherToppings = ['green peppers', 'red onion'];
+  this.otherToppings = [],
   // size
-  this.size = 'medium';
+  this.size = '',
   // cost
-  this.cost = 0;
+  this.cost = 0,
+  // keep track of how many pizzas
+  this.id = 0;
 }
 
-//topping choices
-var toppingChoicesMeat = ['anchovies', 'bacon', 'chicken',  'hamburger', 'pepperoni', 'sauasage']
+Order.prototype.addPizza = function() {
+  var newPizza = new Pizza();
+  $(newOrder.pizzas.push(newPizza));
+}
 
-var toppingChoicesOther = ['green peppers', 'red onion', 'mushrooms', 'olives',]
 
-// size toppingChoices
-var sizeChoices = ['small', 'medium', 'large', 'XL']
-var sizes = [5, 7, 9, 11];
 
-// create a new instance of pizza
-var newPizza = new Pizza();
 
 // cost calculator
-Pizza.prototype.totalCost = function() {
-  var meatCost = 2;
-  var otherCost = 1;
-  console.log(this.cost);
+Pizza.prototype.totalPizzaCost = function() {
+  // size toppingChoices
+  var sizeChoices = ['small', 'medium', 'large', 'XL']
+  var sizes = [5, 7, 9, 11];
   // add to cost based on size
   for (var i = 0; i < sizeChoices.length; i++) {
     if (sizeChoices[i] === this.size) {
@@ -34,17 +40,44 @@ Pizza.prototype.totalCost = function() {
       break;
     }
   }
-  console.log(this.cost);
   // add to cost based on toppings
   for (var i = 0; i < this.meatToppings.length; i++) {
     //if the topping is a meat topping add $2
-    this.cost += meatCost;
-    console.log(this.cost);
+    this.cost += 2;
   };
   for (var i = 0; i < this.otherToppings.length; i++) {
     // if the topping is not a meat topping add $1
-    this.cost += otherCost;
-    console.log(this.cost);
+    this.cost += 1;
   };
   console.log(this.cost);
 }
+
+Pizza.prototype.addTopping = function() {
+  //topping choices
+  var toppingChoicesMeat = ['anchovies', 'bacon', 'chicken',  'hamburger', 'pepperoni', 'sauasage'];
+  var toppingChoicesOther = ['green peppers', 'red onion', 'mushrooms', 'olives',];
+  // determine which type of topping it is
+  topping
+}
+var newOrder = new Order();
+$(document).ready(function() {
+  $('#addPizza').click(function() {
+    newOrder.addPizza();
+  })
+  $('#toppings').click(function() {
+    $('input:checkbox[name=meatTopping]:checked').each(function() {
+      newOrder.pizzas[newOrder.currentPizza].meatToppings.push($(this).val());
+    })
+    $('input:checkbox[name=otherTopping]:checked').each(function() {
+      newOrder.pizzas[newOrder.currentPizza].otherToppings.push($(this).val());
+    })
+    $('#chooseSize').click(function() {
+      newOrder.pizzas[newOrder.currentPizza].size = ($(this).val());
+    })
+    $('#finish').click(function() {
+      newOrder.pizzas[newOrder.currentPizza].totalPizzaCost();
+      newOrder.total += newOrder.pizzas[newOrder.currentPizza].cost;
+      $('h1').text(newOrder.total);
+    })
+  })
+})
